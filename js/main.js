86b6ab112c362b37d675ed936b62ff53d09e84fc
Zav-Xoz <webzavhoz@gmail.com>
2020-07-02 23:56:34 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+console.log(second);
+// var number = 5; infinity NaN
+// var string = "Hello!";
+// var sym = Sumbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj ={};
+
+ let persone = {
+     name: "Jhone",
+     age: 25,
+     isMarried: false
+ };
+
+ console.log(persone.name);
+ console.log(persone["name"]);
+
+ var arr = ['plum.png','orange.png','apple.bmp'];
+
+ console.log(arr[0]);
+ console.log(arr[2]);
